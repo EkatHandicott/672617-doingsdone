324f62455ba7a1484382dd19c413e76f3f0f30b7
Ekat <ekat.handicott@gmail.com>
2019-02-11 19:42:34 +1100

    Database created

---
+++ schema.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE doingsdone
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+USE doingsdone;
+
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title TINYTEXT(100) UNIQUE,
+    author INT NOT NULL UNIQUE
+);
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    dt_compl DATETIME,
+    status BIT DEFAULT 0,
+    title TINYTEXT(100) NOT NULL,
+    file IMAGE,
+    dt_due DATETIME,
+    author INT NOT NULL,
+    project_id INT
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dt_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    email CHAR(60) NOT NULL UNIQUE,
+    name CHAR(60) NOT NULL UNIQUE,
+    password CHAR(25) NOT NULL
+);
+
+
